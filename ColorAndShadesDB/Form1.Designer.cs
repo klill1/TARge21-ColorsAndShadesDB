0ff629b35d82e551eb1ad332dd7cfa0c8f34d809
Kert L <kert.lillemets@gmail.com>
2022-03-13 15:36:09 +0200

    Add ColorAndShadesApp

---
+++ ColorAndShadesDB/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace ColorAndShadesDB
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.listPrimaryColor = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listShades = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(76, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Primary Color";
+            // 
+            // listPrimaryColor
+            // 
+            this.listPrimaryColor.FormattingEnabled = true;
+            this.listPrimaryColor.Location = new System.Drawing.Point(79, 103);
+            this.listPrimaryColor.Name = "listPrimaryColor";
+            this.listPrimaryColor.Size = new System.Drawing.Size(120, 95);
+            this.listPrimaryColor.TabIndex = 1;
+            this.listPrimaryColor.SelectedIndexChanged += new System.EventHandler(this.listPrimaryColor_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(248, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Shades";
+            // 
+            // listShades
+            // 
+            this.listShades.FormattingEnabled = true;
+            this.listShades.Location = new System.Drawing.Point(251, 103);
+            this.listShades.Name = "listShades";
+            this.listShades.Size = new System.Drawing.Size(120, 95);
+            this.listShades.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listShades);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.listPrimaryColor);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listPrimaryColor;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listShades;
+    }
+}
+
---
+++ ColorAndShadesDB/Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ColorAndShadesDB
+{
+    public partial class Form1 : Form
+    {
+        string connectionString;
+        SqlConnection connection;
+        public Form1()
+        {
+            InitializeComponent();
+            connectionString = ConfigurationManager.ConnectionStrings["ColorAndShadesDB.Properties.Settings.ColorsConnectionString"].ConnectionString;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            PopulateColorsTable();
+        }
+
+        private void PopulateColorsTable()
+        {
+            using(connection = new SqlConnection(connectionString))
+            using(SqlDataAdapter adapter = new SqlDataAdapter("SELECT * FROM PrimaryColor", connection))
+            {
+                DataTable colorTable = new DataTable();
+                adapter.Fill(colorTable);
+
+                listPrimaryColor.DisplayMember = "PrimaryColorName";
+                listPrimaryColor.ValueMember = "Id";
+                listPrimaryColor.DataSource = colorTable;
+            }
+        }
+
+        private void listPrimaryColor_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            PopulateColorShades();
+        }
+
+        private void PopulateColorShades()
+        {            
+            string query = "SELECT Shades.ColorName FROM PrimaryColor INNER JOIN Shades ON Shades.PrimaryColorId = PrimaryColor.Id WHERE PrimaryColor.Id = @PrimaryColorId";
+            using (connection = new SqlConnection(connectionString))
+            using (SqlCommand command = new SqlCommand(query, connection))
+            using (SqlDataAdapter adapter = new SqlDataAdapter(command))
+            {
+                command.Parameters.AddWithValue("@PrimaryColorId", listPrimaryColor.SelectedValue);
+                DataTable colorNameTable = new DataTable();
+                adapter.Fill(colorNameTable);
+
+                listShades.DisplayMember = "ColorName";
+                listShades.ValueMember = "Id";
+                listShades.DataSource = colorNameTable;
+            }
+        }
+    }
+}
